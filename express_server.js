1452f450bc3df1401e8ffc7a02bad6b32ccd190b
sarah-zhan <yizh.contact@gmail.com>
2023-02-22 11:01:38 -0800

    install morgan

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const { request } = require("express");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 const generateRandomString = () => {
   return Math.random().toString(36).substring(2, 8);
@@ -15,16 +17,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//browse
 app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls", (req, res) => {
@@ -32,15 +28,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+//read
+
+//create
+app.get("/new", (req, res) => {
+  res.render("urls_new");
 });
 
-app.post('/urls', (req, res) => {
+app.post("/new", (req, res) => {
   const urlId = generateRandomString();
   const longURL = req.body.longURL;
   urlDatabase[urlId] = longURL;
-  res.redirect(`/urls/${urlId}`);
+  res.redirect("/urls");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -53,12 +52,37 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
-app.post('/urls/:id/delete', (req, res) => {
+//edit
+app.get("/urls/edit/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_edit", templateVars);
+});
+
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
+// app.get("/login", (req, res) => {
+//   res.render("/urls");
 
+// });
+
+// app.post("/login", (req, res) => {
+//   const username = res.cookie;
+//   res.send(username);
+//   res.redirect("/urls");
+// });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
