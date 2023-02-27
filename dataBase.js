30558466077f7049869d81d1117888d0715efe67
sarah-zhan <yizh.contact@gmail.com>
2023-02-26 21:06:39 -0800

    move database to a seperate file

---
+++ dataBase.js
@@ -0,0 +1,25 @@
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "userRandomID",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+const users = {
+  u1czze: {
+    id: "u1czze",
+    email: "asdfg@gmail.com",
+    password: "$2a$10$tZtmzd79NimrFqCcm5PdcegVuA1hn93eIDyfQmAyKIs025n82h4T6",
+  },
+  iup9ea: {
+    id: "iup9ea",
+    email: "user@example.com",
+    password: "$2a$10$qHw0E3z4K6f8V/bEexlNke81IpWlPqkTuz9qxvsKLiVDk/aFIUx3G",
+  },
+};
+
+module.exports = { urlDatabase, users };
--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ const app = express();
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcryptjs");
 const { getUserByEmail, urlsForUser, generateRandomString } = require("./helpers");
+const { urlDatabase, users } = require("./dataBase");
 const PORT = 8080;
 
 app.set("view engine", "ejs");
@@ -15,30 +16,6 @@ app.use(cookieSession({
   keys: ["thisisveryfunnyhahahahahaha", "nosleep"]
 }));
 app.use(express.json());
-
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "userRandomID",
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW",
-  },
-};
-
-const users = {
-  u1czze: {
-    id: "u1czze",
-    email: "asdfg@gmail.com",
-    password: "$2a$10$tZtmzd79NimrFqCcm5PdcegVuA1hn93eIDyfQmAyKIs025n82h4T6",
-  },
-  iup9ea: {
-    id: "iup9ea",
-    email: "user@example.com",
-    password: "$2a$10$qHw0E3z4K6f8V/bEexlNke81IpWlPqkTuz9qxvsKLiVDk/aFIUx3G",
-  },
-};
 
 
 //Homepage
